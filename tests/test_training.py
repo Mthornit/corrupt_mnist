61517bbc377fe49bb7a06de1684f1a1574e4335b
mthornit <mthornit@gmail.com>
2026-01-12 22:28:43 +0100

    more attempted fix of actions

--- tests/test_training.py
+++ tests/test_training.py
@@ -1,24 +1,24 @@
 import wandb
 import os
 
-try:
-    from dotenv import load_dotenv
-    load_dotenv()
-    api_key = os.getenv("WANDB_API_KEY")
-    wandb.login(key=api_key)
-except:
-    pass
-
-run = wandb.init(
-# Set the wandb entity where your project will be logged (generally your team name).
-entity="mthornit-team",
-# Set the wandb project where this run will be logged.
-project="corrupt_mnist",
-# Not a new experiment, just reading data
-job_type="read"
-)
-
 def test_traning():
+    try:
+        from dotenv import load_dotenv
+        load_dotenv()
+        api_key = os.getenv("WANDB_API_KEY")
+        wandb.login(key=api_key)
+    except:
+        pass
+
+    run = wandb.init(
+    # Set the wandb entity where your project will be logged (generally your team name).
+    entity="mthornit-team",
+    # Set the wandb project where this run will be logged.
+    project="corrupt_mnist",
+    # Not a new experiment, just reading data
+    mode="offline"
+    )
+    
     # Retrieve the latest version of the artifact
     artifact = wandb.use_artifact('mthornit-team/corrupt_mnist/corrupt_mnist_model:v0', type='model')
 
